326a2013ebd0b01567f05f2046327eead60cfec0
PepeTureatca <aletur4@alu.edu.gva.es>
2026-05-02 11:38:12 +0200

    feat: add favicon to multiple views and include favicon.svg

--- app/views/accountView.php
+++ app/views/accountView.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MusicStream - Cuenta</title>
+    <link rel="icon" type="image/svg+xml" href="assets/favicon.svg">
 
     <link rel="stylesheet" href="/mi-spotify/public/assets/css/dashboard.css">
     <link rel="stylesheet" href="/mi-spotify/public/assets/css/responsiveDashboard.css">
